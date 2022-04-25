d7f38516eee29916db077c5d7005787bc538df2a
oluwadare <wintan1418@gmail.com>
2022-04-25 11:57:31 +0100

    Fix user sign up error

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,7 +4,8 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
 //
+//= require rails-ujs
 //= require jquery
-//= require jquery_ujs
 //= require twitter/bootstrap
+//= require turbolinks
 //= require_tree .
